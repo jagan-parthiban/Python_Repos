08085d050af61dc5fb90bb38d38ca33fd7ff80ac
Jagan Parthiban <jagan.parthiban@happiestminds.com>
2018-10-15 13:30:44 +0530

    app

--- Function_LeapYear.py
+++ Function_LeapYear.py
@@ -4,7 +4,7 @@ def is_leap(year):
         leap = False
     elif (year%4 == 0 and year%100 != 0):
         leap = True
-    elif (year%4 == 0 and year%100 == 0 and year%400 == 0):
+    elif (year%4 == 0 and year%100 == 0 and year%40 == 0):
         leap = True
     elif (year%4 == 0 and year%100 == 0 and year%400 != 0):
         leap = False
